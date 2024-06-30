e360a5c98ffb2b8ff950b7aaa2b340ae30daa12b
김영하 <84363984+younghotkim@users.noreply.github.com>
2024-06-30 23:32:38 +0900

    [level 0] Title: 각도기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,10 @@
+function solution(angle) {
+    if (0<angle && angle <90) {
+        return 1
+    } else if (angle == 90) {
+        return 2
+    } else if (90 < angle && angle < 180) {
+        return 3
+    } else (angle == 180)
+        return 4
+}

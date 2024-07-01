9670deaa5679cb2fd00e397088a0fb649738baff
김영하 <84363984+younghotkim@users.noreply.github.com>
2024-07-01 10:16:36 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,7 @@
+function solution(numer1, denom1, numer2, denom2) {
+  const numer = numer1 * denom2 + numer2 * denom1;
+  const denom = denom1 * denom2;
+  const getGcd = (a,b) => (a%b == 0 ? b : getGcd(b,a%b));
+  const gcd = getGcd(numer, denom); 
+  return [numer / gcd, denom / gcd];
+}

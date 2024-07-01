419d512ceadba6f4516a74b481eaa2fbf80e2e38
김영하 <84363984+younghotkim@users.noreply.github.com>
2024-07-01 10:38:29 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.12 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    
+    var answer = [];
+    
+    for (let i =0; i<numbers.length;i++) {
+        
+        answer.push(numbers[i]*2);
+    }
+        
+    return answer;
+}
